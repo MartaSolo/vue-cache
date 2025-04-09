744cb01add83e4c159d10688d52d74f6040a0d68
Marta Solomacha <marta.solomacha@monterail.com>
2025-04-09 22:01:20 +0200

    Edit components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
-<script setup lang="ts"></script>
-
 <template>
   <main>Homepage</main>
+  <div>{{ data }}</div>
 </template>
+
+<script setup lang="ts">
+import { weatherApiUrl } from '@/api/endpoints';
+import { useFetch } from '@/composables/useFetch';
+
+const { data, fetchData, error, loading } = useFetch(weatherApiUrl);
+
+fetchData();
+</script>
