81f95d6a0b287a7f014443476185287138e07166
Marta Solomacha <marta.solomacha@monterail.com>
2025-04-13 14:36:31 +0200

    Add weather data visualization

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,25 @@
 <template>
   <main>
-    <h1>Homepage</h1>
-    <Loader />
-    <div>{{ data }}</div>
+    <Loader v-if="loading" />
+    <div v-else-if="error">The weather data cannot be displayed</div>
+    <Weather v-else-if="data" :forecast="data" />
+    <div v-else>
+      <span>Something went wrong. Try refreshing</span>
+      <button @click="refresh">Refresh</button>
+    </div>
   </main>
 </template>
 
 <script setup lang="ts">
+import type { WeatherForecast } from '@/types';
 import { weatherApiUrl } from '@/api/endpoints';
 import { useFetch } from '@/composables/useFetch';
 import Loader from '@/components/Loader.vue';
+import Weather from '@/components/Weather.vue';
+import { useCache } from '@/composables/useCache';
 
-const { data, fetchData, error, loading } = useFetch(weatherApiUrl, { ttl: 60000 });
+const { data, error, loading, fromCache, refresh } = useFetch<WeatherForecast>(weatherApiUrl, {
+  ttl: 60000,
+});
+const { cache } = useCache();
 </script>
